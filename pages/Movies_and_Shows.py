c37904a3cc3ea7fb7f678b53922311faa4f37480
Katkov Fedor <pweverj126@gmail.com>
2025-07-17 13:59:21 -0700

    Added new code

--- pages/Movies_and_Shows.py
+++ pages/Movies_and_Shows.py
@@ -8,4 +8,21 @@ with st.sidebar:
     st.title("🎥 Movies vs. Shows")
     st.markdown("Movies vs. Shows battle never ends. On these page, I analyze the given data about movies & shows")
 
+data = pd.read_csv("b.csv")
 
+st.title("First steps")
+st.write("Before we start to analyze the data, we need to divide that into two categories: Movies and Shows:")
+
+
+def movies_analysis():
+    pass
+
+def shows_analysis():
+    pass
+
+
+categories = st.selectbox("Select necessary analysis", ["Movies", "Shows"])
+if categories == "Movies":
+    movies_analysis()
+else:
+    shows_analysis()
