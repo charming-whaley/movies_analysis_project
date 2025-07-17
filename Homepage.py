8d912fbc4724a6a00bfba38a33c726af06cc71b6
Katkov Fedor <pweverj126@gmail.com>
2025-07-17 13:47:39 -0700

    Added new page

---
+++ Homepage.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+url = "https://www.kaggle.com/datasets/sufyan145/netflix-movies-and-shows-imdb-scores"
+
+with st.sidebar:
+    st.title("Homepage")
+    st.markdown("Here you can find some info about the dataset and what I am going to analyze. To explore the dataset, please go to [Kaggle](%s)" % url)
---
+++ pages/Movies_and_Shows.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+
+with st.sidebar:
+    st.title("🎥 Movies vs. Shows")
+    st.markdown("Movies vs. Shows battle never ends. On these page, I analyze the given data about movies & shows")
+
+
