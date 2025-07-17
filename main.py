b83b2fb8fbf08d49fb87e143800bbeeea932581d
Katkov Fedor <pweverj126@gmail.com>
2025-07-17 13:36:54 -0700

    Added sidebar

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.title("Test page")
+url = "https://www.kaggle.com/datasets/sufyan145/netflix-movies-and-shows-imdb-scores"
+
+with st.sidebar:
+    st.title("Homepage")
+    st.markdown("Here you can find some info about the dataset and what I am going to analyze. To explore the dataset, please go to [Kaggle](%s)" % url)
